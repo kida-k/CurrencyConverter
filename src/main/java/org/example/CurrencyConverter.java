59b076e75796d058e689ec203c448b51bc5bf9bb
IKKNIGHT <kidakhanooni1@gmail.com>
2024-07-02 14:53:37 -0500

    conversion functions created

--- src/main/java/org/example/CurrencyConverter.java
+++ src/main/java/org/example/CurrencyConverter.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class CurrencyConverter {
 
-    private static JsonFileReader currency;
+    public static JsonFileReader currency;
 
     public static void main(String[] args) {
         try {
@@ -15,11 +15,27 @@ public class CurrencyConverter {
             System.err.println("Problem with data.json: " + e.getMessage());
             return;
         }
-
-        System.out.println(currency.getData("USD"));
+        CurrencyConverter converter = new CurrencyConverter();
+        double convertedValue = converter.currTocurr("SBD", 10,"INR");
+        System.out.println("Converted value: " + convertedValue);
         // ask for currency to convert from
         // if this is not USD convert to usd
         // then convert to other currency!!!
-
     }
+
+    public double usdConversion(String curr, double value){
+            return currency.getData(curr)*value;
+            //return 1;
+    }
+
+    public double conversionToUSD(String curr, double value){
+        return value/currency.getData(curr);//currency.getData(curr)/value
+
+        //return 1;
+    }
+
+     public double currTocurr(String curr1, double value, String curr2){
+       return usdConversion(curr2,conversionToUSD(curr1,value));
+     }
+
 }
