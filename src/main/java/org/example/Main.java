a5ff271c5e5fe8dcb12377e90692e5936f009bbd
IKKNIGHT <kidakhanooni1@gmail.com>
2024-07-04 12:36:20 -0500

    fixed scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+
         CurrencyConverter converter = new CurrencyConverter();
         Scanner in = new Scanner(System.in);
+
+
+        System.out.println("===========================================");
         System.out.println("What currency would you like to start from?");
         String curr1 = in.nextLine();
-        System.out.println("What currency would you like to conINRvert to?");
-        String curr2 = in.nextLine();
-        System.out.println("How much do you want to convert?");
-        double amount = in.nextDouble();
-        double convertedValue = converter.currToCurr(curr1 , amount, curr2);
-        System.out.println("converted value: " + convertedValue);
+            System.out.println("What currency would you like to convert to?");
+            String curr2 = in.nextLine();
+            System.out.println("How much do you want to convert?");
+            double amount = in.nextDouble();
+            double convertedValue = converter.currToCurr(curr1.toUpperCase() , amount, curr2.toUpperCase());
+            System.out.println("converted value: " + convertedValue);
+            System.out.println("===========================================");
+
+
     }
 
 }
